409b003e53aa41bf5164f3b5d95d31d66cffc1a4
Razze <razzeee@gmail.com>
2024-03-24 00:23:24 +0100

    Restructure pip logic

--- regen-pip.sh
+++ regen-pip.sh
@@ -1,4 +1,3 @@
-rm requirements.txt
 wget https://raw.githubusercontent.com/pgadmin-org/pgadmin4/REL-8_2/requirements.txt
 
 # https://github.com/flatpak/flatpak-builder-tools/issues/365
@@ -6,3 +5,6 @@ cat requirements.txt | grep -v "<= '3.7'" | grep -v "<= '3.10'" | grep -v sys_pl
 sed -i "1 i tomli" requirements_filtered.txt # psycopg-c requires tomli to build
 
 flatpak-pip-generator --yaml -r requirements_filtered.txt --ignore-pkg bcrypt==4.0.* cryptography==41.0.*
+
+rm requirements.txt
+rm requirements_filtered.txt
