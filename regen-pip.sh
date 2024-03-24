b6f38e99c7a55d913faf0e7ded4fbb870bb81176
faveoled <faveoled@yandex.com>
2024-03-24 16:56:59 +0300

    8.4 deps

--- regen-pip.sh
+++ regen-pip.sh
@@ -4,7 +4,7 @@ wget https://raw.githubusercontent.com/pgadmin-org/pgadmin4/REL-8_4/requirements
 cat requirements.txt | grep -v "<= '3.7'" | grep -v "<= '3.10'" | grep -v sys_platform==\"win32\" > requirements_filtered.txt
 sed -i "1 i tomli" requirements_filtered.txt # psycopg-c requires tomli to build
 
-flatpak-pip-generator --yaml -r requirements_filtered.txt --ignore-pkg bcrypt==4.0.* cryptography==41.0.*
+flatpak-pip-generator --yaml -r requirements_filtered.txt --ignore-pkg bcrypt==4.0.* cryptography==42.0.*
 
 rm requirements.txt
 rm requirements_filtered.txt
